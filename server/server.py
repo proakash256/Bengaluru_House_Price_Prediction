387e17024dbf14475c61cdb948f88680958d35ca
Akash <proakash256@gmail.com>
2024-03-22 21:01:40 +0530

    Created the User Interface

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, Request, jsonify
+from flask import Flask, request, jsonify
 import util
 
 app = Flask(__name__)
@@ -13,10 +13,23 @@ def get_location_names():
 
     return response
 
+
 @app.route("/predict_home_price", methods=["POST"])
 def predict_home_price():
-    pass
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
 
 if __name__ == "__main__":
     print("Starting Python Flask Server for Bengaluru Home Price Prediction...")
+    util.load_saved_artifacts()
     app.run()
